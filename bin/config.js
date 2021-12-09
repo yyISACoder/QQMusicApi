a2400b8bddfd93aa5ed199bd10573525faf2395e
yinyue <664960698@qq.com>
2021-12-09 23:31:43 +0800

    add

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  wxuin: 'lihongyu619126', // 如果是微信号的，这里写 wxuin，可以在浏览器端登录后 cookie 中获取
+  wxuin: '1152921505095000202', // 如果是微信号的，这里写 wxuin，可以在浏览器端登录后 cookie 中获取
   port: 3300,
   useDataStatistics: false, // 是否启用数据统计和ip拦截
 }
